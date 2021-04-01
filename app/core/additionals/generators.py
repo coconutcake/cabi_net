120771519f7d0f529a601c03ab80405e623d30f4
coconutcake <contact@mign.pl>
2021-04-01 12:30:12 +0200

    added number gen

--- app/core/additionals/generators.py
+++ app/core/additionals/generators.py
@@ -11,7 +11,7 @@ def password_gen():
     """
 
     while True:
-        p = ''.join(random.choices(string.ascii_letters + string.digits, k=16))
+        p = "".join(random.choices(string.ascii_letters + string.digits, k=16))
         yield p
 
 
@@ -21,17 +21,45 @@ def login_gen():
     """
 
     while True:
-        l = "".join(random.choices(string.ascii_letters.lower() + string.digits, k=random.randint(3, 16)))
+        l = "".join(
+            random.choices(
+                string.ascii_letters.lower() + string.digits, k=random.randint(3, 16)
+            )
+        )
         yield l
+
+
+def custom_number_gen(gen_range: List[int]):
+    """
+    Generates custom number within given range
+    """
+
+    while True:
+        num = random.randint(gen_range[0], gen_range[1])
+        yield num
 
 
 def custom_string_gen(big_letters: bool, digits: bool, gen_range: List[int]):
     """ 
     Generates custom text
     """
-    
+
     while True:
-        l = "".join(random.choices(((string.ascii_letters.lower() if not big_letters else string.ascii_letters) + string.digits if digits else string.ascii_letters), k=random.randint(gen_range[0], gen_range[1])))
+        l = "".join(
+            random.choices(
+                (
+                    (
+                        string.ascii_letters.lower()
+                        if not big_letters
+                        else string.ascii_letters
+                    )
+                    + string.digits
+                    if digits
+                    else string.ascii_letters
+                ),
+                k=random.randint(gen_range[0], gen_range[1]),
+            )
+        )
         yield l
 
 
@@ -42,7 +70,7 @@ def country_gen():
 
     country_codes = [".pl", ".de", ".fr", "co.uk", ".us"]
     # choice = "".join(random.choices(country_codes, k=1))
-    choice = country_codes[random.randint(0,len(country_codes)-1)]
+    choice = country_codes[random.randint(0, len(country_codes) - 1)]
     yield choice
 
 
@@ -52,7 +80,11 @@ def domain_gen():
     """
 
     while True:
-        d = "".join(random.choices(string.ascii_letters.lower() + string.digits, k=random.randint(2,4)))
+        d = "".join(
+            random.choices(
+                string.ascii_letters.lower() + string.digits, k=random.randint(2, 4)
+            )
+        )
         yield d
 
 
@@ -60,9 +92,14 @@ def email_gen():
     """
     Generates email
     """
-    
+
     while True:
-        email = "".join(login_gen().__next__() + "@"+ domain_gen().__next__() + country_gen().__next__())
+        email = "".join(
+            login_gen().__next__()
+            + "@"
+            + domain_gen().__next__()
+            + country_gen().__next__()
+        )
         yield email
 
 
@@ -74,6 +111,6 @@ def user_payload_gen():
     while True:
         user_payload = {
             "email": email_gen().__next__(),
-            "password": password_gen().__next__()
+            "password": password_gen().__next__(),
         }
         yield user_payload
